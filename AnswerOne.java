3abb3ea6cb10954bc180d7741f4daa5c4f2dc885
subish-01 <subishsingh7@gmail.com>
2026-02-03 21:03:25 +0530

    implemented task 1 in java

---
+++ AnswerOne.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class AnswerOne 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        String Name = scan.nextLine();
+        int Age = scan.nextInt();
+        System.out.println(Name);
+        System.out.println(Age);
+    }
+}
