5d81414d8f2594f0b67a2ead384b931797d47a95
subish-01 <subishsingh7@gmail.com>
2026-02-13 22:00:50 +0530

    Displaying array elements using for loop

---
+++ AnswerTwentyThree.java
@@ -0,0 +1,11 @@
+public class AnswerTwentyThree 
+{
+    public static void main(String[] args)
+    {
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        for(int i = 0; i<10;i++)
+        {
+            System.out.println(array[i]);
+        }
+    }
+}
