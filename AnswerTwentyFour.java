990730c650f5889683886a4285ffb27f3c245fd6
subish-01 <subishsingh7@gmail.com>
2026-02-13 22:11:25 +0530

    performed multiplication table using for loop

---
+++ AnswerTwentyFour.java
@@ -0,0 +1,10 @@
+public class AnswerTwentyFour 
+{
+    public static void main(String[] args) 
+    {
+        for(int i =1; i<=10; i++)
+        {
+            System.out.println(i+" x 2 = " +i*2);
+        }
+    }
+}
