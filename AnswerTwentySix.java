98654407e9151717d880ee103ce1a82be9c44e5b
subish-01 <subishsingh7@gmail.com>
2026-02-14 22:07:07 +0530

    Found middle elements of array for both even size and odd size

--- AnswerTwentySix.java
+++ AnswerTwentySix.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class AnswerTwentySix 
@@ -14,6 +15,21 @@ public class AnswerTwentySix
         {
             System.out.print("Enter the Element "+(i+1)+" : ");        
             array[i] = scan.nextInt();                  //getting elements for array
+        }
+        if(size%2 == 0)
+        {
+            int mid1 = (low+high)/2;     // Even sized array has two middle elements 
+            int mid2 = mid1+1;
+            int[] mid1_array = {array[mid1],array[mid2]};
+            System.out.print("The middle elements are: ");
+            System.out.print(Arrays.toString(mid1_array));
+        }
+        else
+        {
+            int mid = (low+high)/2;
+            int[] mid_array = {array[mid]};
+            System.out.print("The middle element is: ");
+            System.out.print(Arrays.toString(mid_array));
         }
     }
 }
