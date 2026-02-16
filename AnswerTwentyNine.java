c00ee35fbbe7b0420e5df2af10821a18d4d85c2f
subish-01 <subishsingh7@gmail.com>
2026-02-16 21:00:42 +0530

    Sum of two numbers using function

---
+++ AnswerTwentyNine.java
@@ -0,0 +1,15 @@
+public class AnswerTwentyNine 
+{
+    int a = 10;
+    int b = 20;
+    void sum()
+    {
+        int c = a+b;
+        System.out.println("The sum of two numbers is "+c);
+    }
+    public static void main(String[] args)
+    {
+        AnswerTwentyNine object = new AnswerTwentyNine();
+        object.sum();
+    }
+}
