6a216e766b836cb1acb9456b76bb8f9c3d7d50b8
subish-01 <subishsingh7@gmail.com>
2026-02-16 20:59:47 +0530

    practiced to write functions

---
+++ AnswerTwentyEight.java
@@ -0,0 +1,29 @@
+public class AnswerTwentyEight 
+{
+    void iphone()
+    {
+        System.out.println("This is an iphone");
+    }
+    void samsung()
+    {
+        System.out.println("This is a samsung mobile");
+    }
+    
+    void realme()
+    {
+        System.out.println("This is a realme mobile");
+    }
+    
+    void oppo()
+    {
+        System.out.println("This is an oppo mobile");
+    }
+    public static void main(String[] args)
+    {
+        AnswerTwentyEight object = new AnswerTwentyEight();
+        object.iphone();
+        object.samsung();
+        object.realme();
+        object.oppo();
+    }
+}
