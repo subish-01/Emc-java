64c57fc5a37642b33f97531972e8b69655c75b25
subish-01 <subishsingh7@gmail.com>
2026-02-17 18:51:37 +0530

    Even or odd number using function

---
+++ AnswerThirtyThree.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class AnswerThirtyThree 
+{
+    void evenorodd(int num)
+    {
+        if(num%2 == 0)
+        {
+            System.out.println("The given number is even");
+        }
+        else
+        {
+            System.out.println("The given number is odd");
+        }
+    }
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = scan.nextInt();
+        AnswerThirtyThree obj = new AnswerThirtyThree();
+        obj.evenorodd(num);
+    }
+}
