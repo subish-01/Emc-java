b9d4e653da230516056a4610122f3880bffc13f9
subish-01 <subishsingh7@gmail.com>
2026-02-18 21:02:48 +0530

    Practiced addition of two numbers using function

---
+++ AnswerThirtyFive.java
@@ -0,0 +1,12 @@
+public class AnswerThirtyFive 
+{
+    void sum(int num1, int num2)
+    {
+        System.out.println("The addition of two numbers is "+(num1+num2));
+    }
+    public static void main(String[] args)
+    {
+        AnswerThirtyFive obj = new AnswerThirtyFive();
+        obj.sum(10,20);
+    }
+}
