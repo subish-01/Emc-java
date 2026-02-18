58f40c66c8b603b67cf7d16ca6e9d4e45e905b40
subish-01 <subishsingh7@gmail.com>
2026-02-18 21:09:35 +0530

    practiced for-each loop in java

---
+++ ForEach.java
@@ -0,0 +1,11 @@
+public class ForEach 
+{
+    public static void main(String[] args)
+    {
+        int[] array = {2,3,3,2,1,4,5,9};
+        for(int n: array)
+        {
+            System.out.print(n);
+        }
+    }
+}
