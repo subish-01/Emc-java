a5613397246175d6139a9ead7fa6aea6738e3e72
subish-01 <subishsingh7@gmail.com>
2026-02-18 21:05:23 +0530

    practiced method overloading

--- AnswerThirtyFive.java
+++ AnswerThirtyFive.java
@@ -4,9 +4,14 @@ public class AnswerThirtyFive
     {
         System.out.println("The addition of two numbers is "+(num1+num2));
     }
+    void sum(int a,int b, int c)
+    {
+        System.out.println("The addition of three numbers are "+(a+b+c));
+    }
     public static void main(String[] args)
     {
         AnswerThirtyFive obj = new AnswerThirtyFive();
         obj.sum(10,20);
+        obj.sum(10,20,30);
     }
 }
